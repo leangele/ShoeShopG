4cd4a2edc0e1b4f9969c6ebd95c2e56c4159a3ca
BlackDaemon <BlackDaemon@DESKTOP-JF3LEH3>
2016-11-24 15:27:03 -0500

    Add project files.

---
+++ ShoeShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoeShop.Startup))]
+namespace ShoeShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
